e06daafcaab99d56198abe29f468f68b54069006
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2024-12-08 22:55:07 +0800

    Day 07 Part 2

--- puzzles/day-07/main.go
+++ puzzles/day-07/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"math"
 )
 
 func readinput(inputfile string) ([]int, [][]int) {
@@ -26,7 +27,7 @@ func readinput(inputfile string) ([]int, [][]int) {
 	return results, operands
 }
 
-func check(res int, operands []int) bool {
+func check1(res int, operands []int) bool {
 	if len(operands) == 0 {
 		return false
 	}
@@ -38,18 +39,62 @@ func check(res int, operands []int) bool {
 		return true
 	}
 
-	if res != 0 && res % last == 0 {
-		return check(res/last, operands[:i]) || check(res-last, operands[:i])
+	if res % last == 0 {
+		return check1(res/last, operands[:i]) || check1(res-last, operands[:i])
 	}
 
-	return check(res-last, operands[:i])
+	return check1(res-last, operands[:i])
+}
+
+func intlen(n int) int {
+	if n == 0 {
+		return 1
+	}
+	len_ := 0
+	for n > 0 {
+		n /= 10
+		len_ += 1
+	}
+	return len_
+}
+
+func checkconcat(res, n int) (bool, int) {
+	l := intlen(n)
+	div := int(math.Pow(float64(10), float64(l)))
+	return res % div == n, div
+}
+
+func check2(res int, operands []int) bool {
+	if len(operands) == 0 {
+		return false
+	}
+
+	i := len(operands)-1
+	last := operands[i]
+
+	if i == 0 && res == last {
+		return true
+	}
+
+	if res % last == 0 {
+		if ok, div := checkconcat(res, last); ok {
+			return check2(res/last, operands[:i]) || check2(res-last, operands[:i]) || check2(res/div, operands[:i])
+		}
+		return check2(res/last, operands[:i]) || check2(res-last, operands[:i])
+	}
+
+	if ok, div := checkconcat(res, last); ok {
+		return check2(res/div, operands[:i]) || check2(res-last, operands[:i])
+	}
+
+	return check2(res-last, operands[:i])
 }
 
 func part1() int {
 	results, operands := readinput("input.txt")
 	sum := 0
 	for i := 0; i < len(results); i++ {
-		if ok := check(results[i], operands[i]); ok {
+		if ok := check1(results[i], operands[i]); ok {
 			sum += results[i]
 		}
 	}
@@ -57,7 +102,14 @@ func part1() int {
 }
 
 func part2() int {
-	return -1
+	results, operands := readinput("input.txt")
+	sum := 0
+	for i := 0; i < len(results); i++ {
+		if ok := check2(results[i], operands[i]); ok {
+			sum += results[i]
+		}
+	}
+	return sum
 }
 
 func main() {
