67a4ba5ba236568344a1436f3f93438da0277ccb
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2024-12-10 00:20:40 +0800

    Day 09 Part 2

--- puzzles/day-09/main.go
+++ puzzles/day-09/main.go
@@ -10,6 +10,10 @@ type block struct {
 	length, free int
 }
 
+type file struct {
+	index, length int
+}
+
 func readinput(inputfile string) []block {
 	input, _ := os.ReadFile(inputfile)
 	stringinput := string(input)
@@ -49,6 +53,43 @@ func compact(fileblocks []string) []string {
 	return fileblocks
 }
 
+func getfiles(fileblocks []string) []file {
+	files := make([]file, len(fileblocks))
+	for i, b := range fileblocks {
+		if b != "." {
+			intb, _ := strconv.Atoi(b)
+			files[intb].length += 1
+			files[intb].index = i-files[intb].length+1
+		}
+	}
+	return files
+}
+
+func compact2(fileblocks []string) []string {
+	files := getfiles(fileblocks)
+	for i := len(files)-1; i >= 0; i-- {
+		file := files[i]
+		if file.length == 0 {
+			continue
+		}
+		freespace := 0
+		for i, b := range fileblocks {
+			if b == "." {
+				freespace += 1
+			} else if f := i-freespace; freespace >= file.length && f < file.index {
+				for offset := 0; offset < file.length; offset++ {
+					fileblocks[f+offset], fileblocks[file.index+offset] = fileblocks[file.index+offset], fileblocks[f+offset]
+				}
+				freespace = 0
+				break
+			} else {
+				freespace = 0
+			}
+		}
+	}
+	return fileblocks
+}
+
 func checksum(compacted []string) int {
 	sum := 0
 	for pos, id := range compacted {
@@ -68,6 +109,15 @@ func part1() int {
 	return sum
 }
 
+func part2() int {
+	diskmap := readinput("input.txt")
+	fileblocks := blocking(diskmap)
+	compacted := compact2(fileblocks)
+	sum := checksum(compacted)
+	return sum
+}
+
 func main() {
 	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
 }
