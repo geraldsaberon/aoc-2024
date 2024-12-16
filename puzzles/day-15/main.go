a63566f3d9ffd04ab2167cce472781057f1fc35d
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2024-12-16 20:49:21 +0800

    Day 15 Part 2

--- puzzles/day-15/main.go
+++ puzzles/day-15/main.go
@@ -34,22 +34,101 @@ func readInput(inputFile string) ([][]string, string, position) {
 	return map_, moves, robotPos
 }
 
-func move(obj *position, dir position, map_ [][]string) bool {
-	nx, ny := obj.x+1*dir.x, obj.y+1*dir.y
-	nextPosition := map_[ny][nx]
-	if nextPosition == "." {
-		map_[ny][nx], map_[obj.y][obj.x] = map_[obj.y][obj.x], map_[ny][nx]
-		obj.x, obj.y = nx, ny
+func widenMap(map_ [][]string) ([][]string, position) {
+	newMap := [][]string{}
+	robotPos := position{}
+	for y := range map_ {
+		row := []string{}
+		for x, s := range map_[y] {
+			if s == "#" {
+				row = append(row, "#", "#")
+			} else if s == "O" {
+				row = append(row, "[", "]")
+			} else if s == "." {
+				row = append(row, ".", ".")
+			} else {
+				row = append(row, "@", ".")
+				robotPos.x, robotPos.y = x*2, y
+			}
+		}
+		newMap = append(newMap, row)
+	}
+	return newMap, robotPos
+}
+
+func move(obj position, dir position, map_ [][]string) bool {
+	np := position{obj.x+dir.x, obj.y+dir.y}
+	npSpot := map_[np.y][np.x]
+	if npSpot == "." {
+		map_[np.y][np.x], map_[obj.y][obj.x] = map_[obj.y][obj.x], map_[np.y][np.x]
 		return true
 	}
-	if nextPosition == "O" {
-		hasMoved := move(&position{nx, ny}, dir, map_)
-		if hasMoved {
-			map_[ny][nx], map_[obj.y][obj.x] = map_[obj.y][obj.x], map_[ny][nx]
-			obj.x, obj.y = nx, ny
+	if npSpot == "O" || npSpot == "[" || npSpot == "]" {
+		if hasMoved := move(position{np.x, np.y}, dir, map_); hasMoved {
+			map_[np.y][np.x], map_[obj.y][obj.x] = map_[obj.y][obj.x], map_[np.y][np.x]
 			return true
 		}
 	}
+	return false
+}
+
+func copyMap(map_ [][]string) [][]string {
+	sCopy := make([][]string, len(map_))
+	for i := range map_ {
+		sCopy[i] = make([]string, len(map_[0]))
+		copy(sCopy[i], map_[i])
+	}
+	return sCopy
+}
+
+func swap(curr, next position, map_ [][]string) {
+	map_[curr.y][curr.x]   = "."
+	map_[curr.y][curr.x+1] = "."
+	map_[next.y][next.x]   = "["
+	map_[next.y][next.x+1] = "]"
+}
+
+func moveBoxUD(obj position, dir position, map_ *[][]string) bool {
+	prevMap := copyMap(*map_)
+
+	np := position{obj.x+dir.x, obj.y+dir.y}
+	L := (*map_)[np.y][np.x]
+	R := (*map_)[np.y][np.x+1]
+
+	if L == "." && R == "." {
+		swap(obj, np, *map_)
+		return true
+	}
+	if L == "]" && R == "." {
+		hasMoved := moveBoxUD(position{np.x-1, np.y}, dir, map_)
+		if hasMoved {
+			swap(obj, np, *map_)
+			return true
+		}
+	}
+	if L == "." && R == "[" {
+		hasMoved := moveBoxUD(position{np.x+1, np.y}, dir, map_)
+		if hasMoved {
+			swap(obj, np, *map_)
+			return true
+		}
+	}
+	if L == "[" && R == "]" {
+		hasMoved := moveBoxUD(position{np.x, np.y}, dir, map_)
+		if hasMoved {
+			swap(obj, np, *map_)
+			return true
+		}
+	}
+	if L == "]" && R == "[" {
+		hasMovedL := moveBoxUD(position{np.x-1, np.y}, dir, map_)
+		hasMovedR := moveBoxUD(position{np.x+1, np.y}, dir, map_)
+		if hasMovedL && hasMovedR {
+			swap(obj, np, *map_)
+			return true
+		}
+	}
+	*map_ = prevMap
 	return false
 }
 
@@ -57,7 +136,7 @@ func getBoxGPSCoordsSum(map_ [][]string) int {
 	sum := 0
 	for y := range map_ {
 		for x, s := range map_[y] {
-			if s == "O" {
+			if s == "O" || s == "[" {
 				sum += 100 * y + x
 			}
 		}
@@ -71,11 +150,55 @@ func part1() int {
 		if m == '\n' {
 			continue
 		}
-		move(&robotPos, directions[m], map_)
+		dir := directions[m]
+		if hasMoved := move(robotPos, dir, map_); hasMoved {
+			robotPos.x += dir.x
+			robotPos.y += dir.y
+		}
+	}
+	return getBoxGPSCoordsSum(map_)
+}
+
+func moveRobot(rp *position, dir position, map_ [][]string) {
+	map_[rp.y][rp.x] = "."
+	rp.x += dir.x
+	rp.y += dir.y
+	map_[rp.y][rp.x] = "@"
+}
+
+func part2() int {
+	map_, moves, rp := readInput("input.txt")
+	map_, rp = widenMap(map_)
+	for _, m := range moves {
+		if m == '\n' {
+			continue
+		}
+		dir := directions[m]
+		np := position{rp.x+dir.x, rp.y+dir.y}
+		npSpot := map_[np.y][np.x]
+		if npSpot == "." {
+			moveRobot(&rp, dir, map_)
+		} else if npSpot == "[" && (m == '^' || m == 'v') {
+			hasMoved := moveBoxUD(np, dir, &map_)
+			if hasMoved {
+				moveRobot(&rp, dir, map_)
+			}
+		} else if npSpot == "]" && (m == '^' || m == 'v') {
+			hasMoved := moveBoxUD(position{np.x-1, np.y}, dir, &map_)
+			if hasMoved {
+				moveRobot(&rp, dir, map_)
+			}
+		} else if (npSpot == "[" || npSpot == "]") && (m == '<' || m == '>') {
+			hasMoved := move(np, dir, map_)
+			if hasMoved {
+				moveRobot(&rp, dir, map_)
+			}
+		}
 	}
 	return getBoxGPSCoordsSum(map_)
 }
 
 func main() {
 	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
 }
