f6f6cbd1c4f291c99a1bbc54b191a0c93cf17268
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2024-12-23 05:10:31 +0800

    Day 22 Part 2

--- puzzles/day-22/main.go
+++ puzzles/day-22/main.go
@@ -33,6 +33,23 @@ func genSecret(secret, nth int) int {
 	return secret
 }
 
+func getPrices(secret, n int) []int {
+	prices := []int{secret % 10}
+	for i := 0; i < n; i++ {
+		secret = genSecret(secret, 1)
+		prices = append(prices, secret % 10)
+	}
+	return prices
+}
+
+func getPriceChanges(prices []int) []int {
+	changes := []int{}
+	for i := 1; i < len(prices); i++ {
+		changes = append(changes, prices[i] - prices[i-1])
+	}
+	return changes
+}
+
 func part1() int {
 	secrets := readInput("input.txt")
 	sum := 0
@@ -42,6 +59,33 @@ func part1() int {
 	return sum
 }
 
+type sequence [4]int
+
+func part2() int {
+	secrets := readInput("input.txt")
+	bananas := map[sequence]int{}
+	for _, s := range secrets {
+		prices := getPrices(s, 2000)
+		priceChanges := getPriceChanges(prices)
+		seen := map[sequence]bool{}
+		for i := 0; i <= len(priceChanges)-4; i++ {
+			seq := sequence(priceChanges[i:i+4])
+			if _, ok := seen[seq]; !ok {
+				bananas[seq] += prices[i+4]
+				seen[seq] = true
+			}
+		}
+	}
+	mostBananas := -1
+	for _, b := range bananas {
+		if b > mostBananas {
+			mostBananas = b
+		}
+	}
+	return mostBananas
+}
+
 func main() {
 	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
 }
